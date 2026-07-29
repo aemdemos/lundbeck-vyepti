4e73c3edcf45c29e6e1754cf47cf94132220d942
Abdulla Shaikh <aashaikh@merkleinc.com>
2026-07-29 17:43:36 +0530

    vyepti Brightcove Vide ESlint issue resolved

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 
 // Function for the brightcove script tag integration for the video rendering
-export function getBrightcoveScriptTag(accountId, playerId) {
+export default function getBrightcoveScriptTag(accountId, playerId) {
   const script = document.createElement('script');
   script.src = `https://players.brightcove.net/${accountId}/${playerId}_default/index.min.js`;
   script.async = true;
